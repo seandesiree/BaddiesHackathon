4a44f91d7d366f96b1f852624c1c5fb72e46482c
seandesiree <sean.desiree@gmail.comgit config --global user.email sean.desiree@gmail.comgit versioncdgit config --global user.email sean.desiree@gmail.com>
2024-03-05 12:49:53 -0500

    try

--- BITchatgpt2.py
+++ BITchatgpt2.py
@@ -7,5 +7,5 @@ load_dotenv()
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 
-completion = client.chat.completions.create(model="gpt-3.5-turbo-0125", messages=[{"role": "user", "content": "For a Hackathon I worked with a team of 5 people to create a custom chatbot. I built the backend using python and Open AI. How do I write this in a cover letter?"}])
+completion = client.chat.completions.create(model="gpt-3.5-turbo-0125", messages=[{"role": "user", "content": "Am I racist?"}])
 print(completion.choices[0].message.content)
